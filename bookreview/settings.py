95cb4e6fb0fdc07024269487a3a7aa813ab131e6
Vedant Gandhi <vedantgandhi24@gmail.com>
2024-07-18 14:49:38 -0700

    added cors headers

--- bookreview/settings.py
+++ bookreview/settings.py
@@ -39,18 +39,19 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "reviews.apps.ReviewsConfig",
     'rest_framework',
-    # 'corsheaders',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # 'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = "bookreview.urls"
@@ -126,4 +127,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://bookreviewsystem.s3-website.us-east-2.amazonaws.com",
+]

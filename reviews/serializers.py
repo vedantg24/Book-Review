320ce3d5c42123b00470de61e2d5732bb5077b52
Vedant Gandhi <vedantgandhi24@gmail.com>
2024-07-18 19:08:49 -0700

    added average rating

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -8,7 +8,14 @@ class ReviewSerializer(serializers.ModelSerializer):
         
 class BookSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
+    average_rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
         fields = '__all__'
+        
+    def get_average_rating(self, obj):
+        reviews = obj.reviews.all()
+        if reviews.count() > 0:
+            return sum(review.rating for review in reviews) / reviews.count()
+        return 0
